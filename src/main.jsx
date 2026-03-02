0449d31e9c3aadf31b38298c514508236d97f140
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2026-03-02 21:32:55 +0100

    Add fetch to pokemon API
    Fetch a list of 10 random pokemons

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+async function fetchPokemons(){
+  try {
+    let response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/?offset=20&limit=10`,
+    );
+    let json = await response.json();
+    return json;
+  } catch {
+    throw new Error("Invalid city name");
+  }
+}
+
+let pokemon = await fetchPokemons()
+console.log(pokemon)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
